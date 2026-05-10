9bbf1b711ca7579982cf211adca15d32a59acd07
Daniel <otagoat14@gmail.com>
2026-05-10 09:34:31 -0600

    [ADD] Instalacion de la libreria SMFL y creacion de una ventana

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
 
-int main () {
+int main() {
+    sf::RenderWindow ventana(
+        sf::VideoMode(800, 600),
+        "Tank Attack - Prueba SFML"
+    );
+
+    while (ventana.isOpen()) {
+        sf::Event evento;
+        while (ventana.pollEvent(evento)) {
+            if (evento.type == sf::Event::Closed)
+                ventana.close();
+        }
+
+        ventana.clear(sf::Color(50, 50, 50));
+        ventana.display();
+    }
     return 0;
 }
